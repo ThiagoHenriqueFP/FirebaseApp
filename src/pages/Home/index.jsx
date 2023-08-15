4958e25602e0e1519d8573f70c7a247f7ae8a194
Thiago Henrique <thiagohp070@gmail.com>
2023-08-15 13:13:52 -0300

    fix: directory updates

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,1 @@
-import "./style.css";
-
-export default function Home() {
-  return (
-    <div className="container">
-      <div>
-        <h1 className="tiny-300 secondary">
-          <span className="italic bold-800 primary" color="#3D246C">
-            My
-          </span>
-          Tracks
-        </h1>
-        <p>Organize as suas musicas</p>
-      </div>
-      <button>Login com google</button>
-    </div>
-  );
-}
+export default function Login() {}
