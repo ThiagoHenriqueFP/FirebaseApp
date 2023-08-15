b898376e247b3d9280074e2475ae920d021cf438
Thiago Henrique <thiagohp070@gmail.com>
2023-08-15 13:17:05 -0300

    fix: directory updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Login from "./pages/Home/index.jsx";
+import Login from "./pages/Login/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
